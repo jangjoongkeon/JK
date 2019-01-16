12ed5beec2c3cb5484c3e77cb970e2e60158f955
jangjoongkeon <45579219+jangjoongkeon@users.noreply.github.com>
2019-01-16 16:46:20 +0900

    Add files via upload

---
+++ func_test3.py
@@ -0,0 +1,5 @@
+def sum(a,b=30):                # b=30, 초기값을 설정해줄 수 있다.
+    return a+b                  # 파이썬 개발자들이  개행을 초기값으로 해놓았다.
+
+print(sum(10))
+print("Hello", end="gfdgdfgdfg ")       #람다라는 명령어는 함수의 코드를 더욱 간단히 쓸 수 있게한다.
